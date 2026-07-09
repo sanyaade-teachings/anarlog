1d28a094ee2385c388d1fdb7d205a580edc58fc6
John Jeong <63365510+ComputelessComputer@users.noreply.github.com>
2026-07-09 14:45:27 +0900

    Thicken popover outer border (#5954)
    Set app popover content to use a 3px outer border.

--- packages/ui/src/components/ui/popover.tsx
+++ packages/ui/src/components/ui/popover.tsx
@@ -37,7 +37,7 @@ const PopoverContent = React.forwardRef<
         className={cn([
           "text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) outline-hidden",
           variant === "app"
-            ? appFloatingContentClassName
+            ? cn([appFloatingContentClassName, "border-[3px]"])
             : "bg-popover rounded-2xl border p-4 shadow-md",
           className,
         ])}
